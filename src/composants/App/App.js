876be5948ba2c8b5623de9b0fd9e8faadc8c1dd1
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-23 11:53:37 +0200

    add auto binding

--- src/composants/App/App.js
+++ src/composants/App/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 
+import "./App.css";
+
+const autoBind = require("auto-bind");
+
 class App extends Component {
+    constructor() {
+        super();
+        autoBind(this);
+    }
+
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>Jeu du pendu</h1>
             </div>
         );
